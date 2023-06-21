0ac240d3a209f99f13bd982a4a164355bc5cc949
Yasmin Fadel <110503796+YasminFadel15@users.noreply.github.com>
2023-06-21 09:33:16 -0300

    Update metodos.c

--- metodos.c
+++ metodos.c
@@ -1,361 +1,302 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
-int mergesort(int[], int);
-void _mergesort(int[], int, int, int*);
-void merge(int[], int, int, int, int*);
-void _mergesortBU(int[], int, int, int*);
-
-int quicksort(int[], int);
-void _quicksort(int[], int, int, int*);
-int partition(int[], int, int, int*);
-
-int radixsort(int[], int);
-void countingsort(int[], int, int, int*);
-int max(int[], int);
-
-int* populaVetorMelhorCaso(int n) {
-    int *v = malloc(sizeof(int) * n);
-    int i;
-    for (i = 0; i < n; i++) {
-        v[i] = i;
-    }
-    return v;
+//função para gerar números inteiros aleatórios entre min e max, contando com eles
+int gerarNumeroAleatorio(int min, int max) {
+    return min + rand() % (max - min + 1);
 }
 
-int* populaVetorPiorCaso(int n) {
-    int *v = malloc(sizeof(int) * n);
-    int i;
-    for (i = 0; i < n; i++) {
-        v[i] = n - i;
-    }
-    return v;
+//implementação dos algoritmos de ordenação
+
+void troca(int *vetor, int i, int j) {
+  int temp = vetor[i];
+  vetor[i] = vetor[j];
+  vetor[j] = temp;
 }
 
-int* populaVetorCasoMedio(int n) {
-    int *v = malloc(sizeof(int) * n);
-    int i;
-    for (i = 0; i < n; i++) {
-        v[i] = rand() % n;
-    }
-    return v;
-}
+void bubbleSort2(int vetor[], int n, int *trocas, int *comparacoes) {
+  int i, trocou;
+  *trocas = 0;
+  *comparacoes = 0;
 
-void troca(int* v, int i, int j) {
-    int aux = v[i];
-    v[i] = v[j];
-    v[j] = aux;
-}
-
-int bubblesort(int* v, int n) {
-    int i,j,c = 0;
-    for (i = 0; i < n - 1; i++) {
-        for (j = i + 1; j < n; j++) {
-            c++; //comparação
-            if (v[i] > v[j]) {
-              troca(v, i, j);
-              c++; //troca
-            }
-        }
-    }
-    return c;
-}
-
-int bubblesort2(int v[], int n) {
-  int i, c, trocou;
   do {
     trocou = 0; // false
     for (i = 0; i < n - 1; i++) {
-      c++;
-      if (v[i] > v[i + 1]) {
-        c++;
-        troca(v, i, i + 1);
-        trocou = 1; // true 
+      (*comparacoes)++;
+      if (vetor[i] > vetor[i + 1]) {
+        (*comparacoes)++;
+        troca(vetor, i, i + 1);
+        trocou = 1; // true
       }
     }
   } while (trocou);
-  return c;
 }
 
-void exibeVetor(int* v, int n) {
-    int i;
-    printf("[ ");
-    for (i = 0; i < n; i++) {
-        printf("%d ", v[i]);
+void insertionSort(int vetor[], int n, int *trocas, int *comparacoes) {
+    *trocas = 0;
+    *comparacoes = 0;
+
+    for (int i = 1; i < n; i++) {
+        int pivo = vetor[i];
+        int j = i - 1;
+        (*comparacoes)++;
+        while (j >= 0 && pivo < vetor[j]) {
+            (*trocas)++;
+            vetor[j + 1] = vetor[j];
+            j--;
+            (*comparacoes)++;
+        }
+        (*trocas)++;
+        vetor[j + 1] = pivo;
     }
-    printf("]\n");
 }
 
-int* copiaVetor(int* v, int n) {
-    int *copia = malloc(sizeof(int) * n);
-    int i;
-    for (i = 0; i < n; i++) {
-        copia[i] = v[i];
-    }
-    return copia;
-}
-
-void heapify(int v[], int n, int i, int* c) {
+void heapify(int vetor[], int n, int i, int *trocas, int *comparacoes) {
     int raiz = i;
     int esquerda = 2 * i + 1;
     int direita = 2 * i + 2;
-    
-    (*c)++;
-    if (esquerda < n && v[esquerda] > v[raiz]) {
+
+    (*comparacoes)++;
+    if (esquerda < n && vetor[esquerda] > vetor[raiz])
         raiz = esquerda;
-    }
 
-    (*c)++;
-    if (direita < n && v[direita] > v[raiz]) {
+    (*comparacoes)++;
+    if (direita < n && vetor[direita] > vetor[raiz])
         raiz = direita;
-    }
-    
+
     if (raiz != i) {
-        (*c)++;
-        troca(v, i, raiz);
-        heapify(v, n, raiz, c);
+        (*trocas)++;
+        int temp = vetor[i];
+        vetor[i] = vetor[raiz];
+        vetor[raiz] = temp;
+        heapify(vetor, n, raiz, trocas, comparacoes);
     }
 }
 
-int heapsort(int v[], int n) {
-    int c = 0;
-    int i;
-    for (i = n / 2 - 1; i >= 0; i--) {
-        heapify(v, n, i, &c);
-    }
-    
-    for (i = n - 1; i > 0; i--) {
-        c++;
-        troca(v, 0, i);
-        heapify(v, i, 0, &c);
-    }
+void heapSort(int vetor[], int n, int *trocas, int *comparacoes) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(vetor, n, i, trocas, comparacoes);
 
-    return c;
+    for (int i = n - 1; i > 0; i--) {
+        (*trocas)++;
+        int temp = vetor[0];
+        vetor[0] = vetor[i];
+        vetor[i] = temp;
+        heapify(vetor, i, 0, trocas, comparacoes);
+    }
 }
 
-int insertionsort(int v[], int n) {
-    int pivo, j, c = 0;
-    for (int i = 1; i < n; i++) {
-        pivo = v[i];
-        j = i - 1;
-        c++; //comparação
-        while (j >= 0 && pivo < v[j]) {
-            c++; // troca
-            v[j + 1] = v[j];  // desloca para a direita
-            j--;
-            c++; // comparação
+void merge(int vetor[], int esquerda, int meio, int direita, int *trocas, int *comparacoes) {
+    int n1 = meio - esquerda + 1;
+    int n2 = direita - meio;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = vetor[esquerda + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = vetor[meio + 1 + j];
+
+    int i = 0;
+    int j = 0;
+    int k = esquerda;
+
+    while (i < n1 && j < n2) {
+        (*comparacoes)++;
+        if (L[i] <= R[j]) {
+            (*trocas)++;
+            vetor[k] = L[i];
+            i++;
+        } else {
+            (*trocas)++;
+            vetor[k] = R[j];
+            j++;
         }
-        c++; // troca
-        v[j + 1] = pivo;
-    }
-    return c;
-}
-
-void merge(int v[], int inicio, int meio, int fim, int* c) {
-    int aux[fim - inicio + 1];
-    int i = inicio, j = meio + 1, k = 0;
-
-    (*c)++;
-    while (i <= meio && j <= fim) { // Intercala
-        aux[k++] = v[i] <= v[j] ? v[i++] : v[j++];
-        (*c)++;
+        k++;
     }
 
-    (*c)++;
-    while (i <= meio) {
-        aux[k++] = v[i++];
-        (*c)++;
-    } 
-
-    (*c)++;
-    while (j <= fim) {
-        aux[k++] = v[j++];
-        (*c)++;
+    while (i < n1) {
+        (*trocas)++;
+        vetor[k] = L[i];
+        i++;
+        k++;
     }
 
-    (*c)++;
-    for (i = inicio, k = 0; i <= fim; i++, k++) {
-        v[i] = aux[k];
-        (*c)++;
+    while (j < n2) {
+        (*trocas)++;
+        vetor[k] = R[j];
+        j++;
+        k++;
     }
 }
 
-int mergesort(int v[], int n) {
-    int c = 0;
-    _mergesort(v, 0, n - 1, &c);
-    return c;
-}   
+void mergeSort(int vetor[], int esquerda, int direita, int *trocas, int *comparacoes) {
+    if (esquerda < direita) {
+        int meio = esquerda + (direita - esquerda) / 2;
 
-void _mergesort(int v[], int inicio, int fim, int* c) {
-    int meio = (inicio + fim) / 2;
-        
-    if (inicio < fim) {
-        _mergesort(v, inicio, meio, c);
-        _mergesort(v, meio + 1, fim, c);
-            
-        merge(v, inicio, meio, fim, c);
+        mergeSort(vetor, esquerda, meio, trocas, comparacoes);
+        mergeSort(vetor, meio + 1, direita, trocas, comparacoes);
+
+        merge(vetor, esquerda, meio, direita, trocas, comparacoes);
     }
 }
 
-int mergesortBU(int v[], int n) {
-  int c = 0;
-  _mergesortBU(v, 0, n - 1, &c);
-  return c;
-}
-
-void _mergesortBU(int v[], int inicio, int fim, int* c) {
-  int i, meio;
-  for (meio = 1; meio < fim - inicio + 1; meio *= 2 * meio) { 
-    for (i = inicio; i <= fim - meio; i += 2 * meio) {
-      int j = (i + (2 * meio)) - 1;
-      if (j > fim) j = fim;
-      
-      merge(v, i, i + meio - 1, j, c);
-    }
-  }
-}
-
-int quicksort(int v[], int n) {
-    int c = 0;
-    _quicksort(v, 0, n - 1, &c);
-    return c;
-}
-
-void _quicksort(int v[], int inicio, int fim, int* c) {
-    if (inicio < fim) {
-        int meio = partition(v, inicio, fim, c);
-                
-        _quicksort(v, inicio, meio - 1, c);  // Antes do pivô
-        _quicksort(v, meio + 1, fim, c); // Após pivô
-    }
-}
-
-int partition(int v[], int inicio, int fim, int* c) {
-    int pivo = v[fim]; // Pivô será o último elemento da partição
-    int i = (inicio - 1); // Indica a posição correta do pivô até então
+int partition(int vetor[], int inicio, int fim, int *trocas, int *comparacoes) {
+    int pivo = vetor[fim];
+    int i = (inicio - 1);
 
     for (int j = inicio; j <= fim - 1; j++) {
-        (*c)++;
-        if (v[j] < pivo) { // Verifica se o elemento atual é menor que o pivô
-            i++; // Incrementa o índice da posição correta do pivô
-            (*c)++;
-            troca(v, i, j); // Realiza a troca (swap) do elemento atual 
+        (*comparacoes)++;
+        if (vetor[j] < pivo) {
+            i++;
+            (*trocas)++;
+            int temp = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = temp;
         }
     }
-    (*c)++;
-    troca(v, i + 1, fim); // Posiciona o pivô na posição correta
-    return i + 1; // Informa o índice a ser utilizado para o próximo particionamento
+
+    (*trocas)++;
+    int temp = vetor[i + 1];
+    vetor[i + 1] = vetor[fim];
+    vetor[fim] = temp;
+
+    return (i + 1);
 }
 
-void countingsort(int v[], int n, int exp, int* c) {
-    int saida[n], frequencia[10]={}; // Dígitos de 0 a 9
-    
-    (*c)++;
-    for (int i = 0; i < n; i++) { // Armazena a frequência de cada valor pelo índice
-        frequencia[(v[i] / exp) % 10]++;
-        (*c)++;
-    }
-    (*c)++;
-    
-    for (int i = 1; i < 10; i++) { // Armazena a soma do elemento atual com o elemento anterior
-        frequencia[i] += frequencia[i - 1];
-        (*c)++;
-    }
-    (*c)++;
-        
-    for (int i = n - 1; i >= 0; i--) { // Cria a saída a partir do vetor de entrada e da frequência
-        (*c)++;
-        saida[frequencia[(v[i] / exp) % 10] - 1] = v[i];
-        frequencia[(v[i] / exp) % 10]--;
-    }
-    (*c)++;
-    
-    for (int i = 0; i < n; i++) { // Copia o vetor de saída para o vetor de entrada
-        v[i] = saida[i];
-        (*c)++;
+void quickSort(int vetor[], int inicio, int fim, int *trocas, int *comparacoes) {
+    if (inicio < fim) {
+        int pi = partition(vetor, inicio, fim, trocas, comparacoes);
+
+        quickSort(vetor, inicio, pi - 1, trocas, comparacoes);
+        quickSort(vetor, pi + 1, fim, trocas, comparacoes);
     }
 }
 
-int max(int v[], int n) {
-    int max = v[0];
-    
-    for (int i = 1; i < n; i++)
-        if (v[i] > max)
-            max = v[i];
+int getMax(int vetor[], int n) {
+    int max = vetor[0];
+    for (int i = 1; i < n; i++) {
+        if (vetor[i] > max)
+            max = vetor[i];
+    }
     return max;
 }
 
-int radixsort(int v[], int n) {
-    int c = 0;
-    int m = max(v, n); // Obtém o maior número para conhecer a quantidade de dígitos
-    
-    for (int exp = 1; m / exp > 0; exp *= 10) //Realiza o counting sort para cada dígito
-        countingsort(v, n, exp, &c); // exp é atualizada com 10^i, onde i é o dígito atual 10, 100, 1000, ...
-    return c;
-}
+void countingSort(int vetor[], int n, int exp, int *trocas, int *comparacoes) {
+    int saida[n];
+    int frequencia[10] = {0};
 
+    for (int i = 0; i < n; i++)
+        frequencia[(vetor[i] / exp) % 10]++;
 
-int main() {
-    int n = 1000;
-    int repeticoes = 30;
-    int i;
-    
-    unsigned int operacoesBubblesort = 0;
-    unsigned int operacoesBubblesort2 = 0;
-    unsigned int operacoesHeapsort = 0;
-    unsigned int operacoesInsertionSort = 0;
-    unsigned int operacoesMergeSort = 0;
-    unsigned int operacoesMergeSort2 = 0;
-    unsigned int operacoesQuickSort = 0;
-    unsigned int operacoesRadixSort = 0;
+    for (int i = 1; i < 10; i++)
+        frequencia[i] += frequencia[i - 1];
 
-    for (i = 0; i < repeticoes; i++) {
-        int* casoMedio = populaVetorCasoMedio(n);
-
-        operacoesBubblesort += bubblesort(copiaVetor(casoMedio, n), n);
-        operacoesBubblesort2 += bubblesort2(copiaVetor(casoMedio, n), n);
-        operacoesHeapsort += heapsort(copiaVetor(casoMedio, n), n);
-        operacoesInsertionSort += insertionsort(copiaVetor(casoMedio, n), n);
-        operacoesMergeSort += mergesort(copiaVetor(casoMedio, n), n);
-        operacoesMergeSort2 += mergesortBU(copiaVetor(casoMedio, n), n);
-        operacoesQuickSort += quicksort(copiaVetor(casoMedio, n), n);
-        operacoesRadixSort += radixsort(copiaVetor(casoMedio, n), n);
+    for (int i = n - 1; i >= 0; i--) {
+        saida[frequencia[(vetor[i] / exp) % 10] - 1] = vetor[i];
+        frequencia[(vetor[i] / exp) % 10]--;
+        (*trocas)++;
     }
 
-    operacoesBubblesort /= repeticoes;
-    operacoesBubblesort2 /= repeticoes;
-    operacoesHeapsort /= repeticoes;
-    operacoesInsertionSort /= repeticoes;
-    operacoesMergeSort /= repeticoes;
-    operacoesMergeSort2 /= repeticoes;
-    operacoesQuickSort /= repeticoes;
-    operacoesRadixSort /= repeticoes;
+    for (int i = 0; i < n; i++) {
+        vetor[i] = saida[i];
+        (*comparacoes)++;
+    }
+}
 
-    printf("-------------------------------\n");
-    printf("Bubblesort\n");
-    printf("Caso medio: %d\n", operacoesBubblesort);
-    printf("-------------------------------\n");
-    printf("Bubblesort 2\n");
-    printf("Caso medio: %d\n", operacoesBubblesort2);
-    printf("-------------------------------\n");
-    printf("Heapsort\n");
-    printf("Caso medio: %d\n", operacoesHeapsort);
-    printf("-------------------------------\n");
-    printf("Insertionsort\n");
-    printf("Caso medio: %d\n", operacoesInsertionSort);
-    printf("-------------------------------\n");
-    printf("Mergesort\n");
-    printf("Caso medio: %d\n", operacoesMergeSort);    
-    printf("-------------------------------\n");
-    printf("MergesortBU\n");
-    printf("Caso medio: %d\n", operacoesMergeSort2);  
-    printf("-------------------------------\n");
-    printf("Quicksort\n");
-    printf("Caso medio: %d\n", operacoesQuickSort);    
-    printf("-------------------------------\n");
-    printf("Radixsort\n");
-    printf("Caso medio: %d\n", operacoesRadixSort);    
-    printf("-------------------------------\n");
-    
+void radixSort(int vetor[], int n, int *trocas, int *comparacoes) {
+    int max = getMax(vetor, n);
+
+    for (int exp = 1; max / exp > 0; exp *= 10)
+        countingSort(vetor, n, exp, trocas, comparacoes);
+}
+
+int main() {
+    srand(time(0)); //inicializa a semente do gerador de números aleatórios
+
+    int tamanho_minimo = 1;
+    int tamanho_maximo = 1000;
+    int num_execucoes = 30;
+
+    //cria um vetor para armazenar os resultados com os 6 métodos
+    int resultados[tamanho_maximo][6]; 
+
+    //loop para os diferentes tamanhos do conjunto de dados
+    for (int tamanho = tamanho_minimo; tamanho <= tamanho_maximo; tamanho++) {
+        int trocas[6] = {0}; //contagem de trocas para cada método de ordenação
+        int comparacoes[6] = {0}; //contagem de comparações para cada método de ordenação
+
+        //loop para execuções distintas
+        for (int execucao = 0; execucao < num_execucoes; execucao++) {
+            int dados[tamanho];
+
+            //gera números aleatórios
+            for (int i = 0; i < tamanho; i++)
+                dados[i] = gerarNumeroAleatorio(1, 1000);
+
+            //faz uma cópia dos dados para cada método de ordenação
+            int dados_copy[6][tamanho];
+            for (int i = 0; i < 6; i++)
+                memcpy(dados_copy[i], dados, tamanho * sizeof(int));
+
+            //bubble sort
+            bubbleSort2(dados_copy[0], tamanho, &trocas[0], &comparacoes[0]);
+
+            //insertion sort
+            insertionSort(dados_copy[1], tamanho, &trocas[1], &comparacoes[1]);
+
+            //heap sort
+            heapSort(dados_copy[2], tamanho, &trocas[2], &comparacoes[2]);
+
+            //merge sort
+            mergeSort(dados_copy[3], 0, tamanho - 1, &trocas[3], &comparacoes[3]);
+
+            //quick sort
+            quickSort(dados_copy[4], 0, tamanho - 1, &trocas[4], &comparacoes[4]);
+
+            //radix sort
+            radixSort(dados_copy[5], tamanho, &trocas[5], &comparacoes[5]);
+        }
+
+        //calcula a média das trocas e comparações
+        for (int i = 0; i < 6; i++) {
+            trocas[i] /= num_execucoes;
+            comparacoes[i] /= num_execucoes;
+        }
+
+        //armazena os resultados
+        for (int i = 0; i < 6; i++) {
+            resultados[tamanho - 1][i] = trocas[i] + comparacoes[i];
+        }
+    }
+
+    //abre o arquivo para escrever os resultados em formato CSV
+    FILE *csvFile = fopen("resultadosMetodos_.csv", "w");
+    if (csvFile == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    //escreve o cabeçalho 
+    fprintf(csvFile, "Tamanho,Bubble Sort,Insertion Sort,Heap Sort,Merge Sort,Quick Sort,Radix Sort\n");
+
+    //escreve os resultados 
+    for (int tamanho = tamanho_minimo; tamanho <= tamanho_maximo; tamanho++) {
+        fprintf(csvFile, "%d,", tamanho);
+        for (int i = 0; i < 6; i++) {
+            fprintf(csvFile, "%d", resultados[tamanho - 1][i]);
+            if (i < 5)
+                fprintf(csvFile, ",");
+        }
+        fprintf(csvFile, "\n");
+    }
+
+    //fecha o arquivo
+    fclose(csvFile);
+
+    printf("Arquivo 'resultados.csv' gerado com sucesso!\n");
+
     return 0;
 }
